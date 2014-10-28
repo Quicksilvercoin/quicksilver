96985515a20a1ee0d117e177c1695daf0195123c
Quicksilvercoin <quicksilvercoin@gmail.com>
2014-10-28 03:42:44 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Quicksilver cryptocurrency.
+
+5 Million Coins
+
+6% POS Interest per Year
+
+Min Stake Age 4 Hours
+
+Max Stake Age 20 Days
+
+RPCPort: 21334
+
+P2PPort: 21333
+
+POD Cryptoasian
+
+
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define quicksilver-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "quicksilverURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
