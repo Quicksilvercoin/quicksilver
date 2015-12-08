e88b9cef0d7832bf155c1dfa8f43d91c8b0a1011
Luke Williams <luke@iquidus.io>
2015-12-08 14:51:10 +1300

    re-enable PoW

--- src/main.h
+++ src/main.h
@@ -26,7 +26,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const int LAST_POW_TIME = 1415620800;
+static const int LAST_POW_TIME = 1464739200; // Wed, 01 Jun 2016 00:00:00 +0000
 
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
